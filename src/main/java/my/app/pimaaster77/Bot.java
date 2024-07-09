218fb9dfc25da2a8934ed87c941a29825ae051f6
pi-maaster77 <60481254+pi-maaster77@users.noreply.github.com>
2024-07-09 15:34:10 +0000

    se añadio el sistema de Ngrok

--- src/main/java/my/app/pimaaster77/Bot.java
+++ src/main/java/my/app/pimaaster77/Bot.java
@@ -1,25 +1,31 @@
 package my.app.pimaaster77;
 
 import org.bukkit.plugin.java.JavaPlugin;
-
+import discord4j.common.util.Snowflake;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
 
-
 import java.io.File;
 
 public class Bot extends JavaPlugin {
     private GatewayDiscordClient discordClient;
+    private static NgrokTunnel tunnel = new NgrokTunnel();
 
     @Override
     public void onEnable() {
         getLogger().info("TuPlugin ha sido habilitado");
         loadConfig();
-
-        String token = getConfig().getString("token");  // Reemplaza con tu token de Discord
+        String tunnelUrl = tunnel.getNgrokTunnelUrl(getServer().getPort());
+        if (tunnelUrl == null || tunnelUrl.isEmpty()) {
+            getLogger().severe("No se pudo obtener la URL del túnel de ngrok");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        String token = getConfig().getString("token");
+        String anunciosID = getConfig().getString("anunciosID");
 
         if (token == null || token.isEmpty()) {
             getLogger().severe("No token found in config.yml!");
@@ -30,12 +36,24 @@ public class Bot extends JavaPlugin {
         DiscordClient client = DiscordClient.create(token);
         discordClient = client.login().block();
 
+        // Enviar un mensaje al canal de anuncios cuando el bot se habilite
+        if (anunciosID != null && !anunciosID.isEmpty()) {
+            discordClient.getChannelById(Snowflake.of(anunciosID))
+                .ofType(MessageChannel.class)
+                .flatMap(channel -> channel.createMessage("```" + tunnelUrl + "```"))
+                .block();
+        } else {
+            getLogger().severe("No anunciosID found in config.yml!");
+        }
+
         discordClient.getEventDispatcher().on(MessageCreateEvent.class)
             .subscribe(event -> {
                 Message message = event.getMessage();
                 if (message.getContent().equalsIgnoreCase("!ping")) {
                     MessageChannel channel = message.getChannel().block();
-                    channel.createMessage("Pong!").block();
+                    if (channel != null) {
+                        channel.createMessage("Pong!").block();
+                    }
                 }
             });
 
@@ -47,6 +65,7 @@ public class Bot extends JavaPlugin {
         if (discordClient != null) {
             discordClient.logout().block();
         }
+        tunnel.closeNgrokTunnel();
         getLogger().info("TuPlugin ha sido deshabilitado");
     }
 
